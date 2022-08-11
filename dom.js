e1c02a0ccb9b9f462707745ba2a39151c566658a
Shaheer Khan NS <khanshaheer43@gmail.com>
2022-08-11 10:30:10 +0530

    First Commit

---
+++ dom.js
@@ -0,0 +1,9 @@
+
+
+const header = document.getElementById('main-header');
+
+header.style.borderBottom='solid 3px black';
+const title = document.getElementById('header-title');
+
+title.style.fontFamily='times new roman';
+title.style.color='red';
